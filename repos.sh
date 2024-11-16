165588306be53b492a8c2c31371d4df7a39cf212
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-11-16 14:50:21 +0000

    Update repos.sh - added sneat-co/sneat-firebase

--- repos.sh
+++ repos.sh
@@ -1,7 +1,12 @@
 #!/bin/zsh
 
+# At least 3 repos are required to run Sneat.app locally
 echo "sneat-co/sneat-apps"
-echo "sneat-co/sneat-go-backend"
+echo "sneat-co/sneat-firebase"
+echo "sneat-co/sneat-go-server"
+
+# This backend repose are most likely to be looked at and to be modified during development
 echo "sneat-co/sneat-go-core"
 echo "sneat-co/sneat-core-modules"
 echo "sneat-co/sneat-go-modules"
+echo "sneat-co/sneat-go-backend"
