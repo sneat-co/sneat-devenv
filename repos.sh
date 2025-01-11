5e0e01a11acdcb30890538a4dc39109ce8c269db
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2025-01-11 16:46:33 +0000

    chore: echo "sneat-co/sneat-firebase" # Firebase project & emulator

--- repos.sh
+++ repos.sh
@@ -7,5 +7,5 @@ echo "sneat-co/sneat-go-backend" # Holds extra modules
 
 # At least 3 repos are required to run Sneat.app locally
 echo "sneat-co/sneat-apps" # Frontend (Angular app)
-echo "sneat-co/sneat-firebase" # Binding for DALgo to Firebase specific for Sneat backend
+echo "sneat-co/sneat-firebase" # Firebase project & emulator
 echo "sneat-co/sneat-go-server" # Runs Sneat backend server locally
