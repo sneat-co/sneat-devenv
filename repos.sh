42b978a3e5419cfc3d4a99698138bfd9106a05c0
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2025-01-11 16:23:21 +0000

    fix: removes sneat-go-modules and reorders repos

--- repos.sh
+++ repos.sh
@@ -1,12 +1,11 @@
 #!/bin/zsh
 
-# At least 3 repos are required to run Sneat.app locally
-echo "sneat-co/sneat-apps"
-echo "sneat-co/sneat-firebase"
-echo "sneat-co/sneat-go-server"
-
 # This backend repose are most likely to be looked at and to be modified during development
-echo "sneat-co/sneat-go-core"
-echo "sneat-co/sneat-core-modules"
-echo "sneat-co/sneat-go-modules"
-echo "sneat-co/sneat-go-backend"
+echo "sneat-co/sneat-go-core" # Core packages for Sneat backend
+echo "sneat-co/sneat-core-modules" # Core business modules for Sneat backend
+echo "sneat-co/sneat-go-backend" # Holds extra modules
+
+# At least 3 repos are required to run Sneat.app locally
+echo "sneat-co/sneat-apps" # Frontend (Angular app)
+echo "sneat-co/sneat-firebase" # Binding for DALgo to Firebase specific for Sneat backend
+echo "sneat-co/sneat-go-server" # Runs Sneat backend server locally
